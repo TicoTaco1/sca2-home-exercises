78ff4d06a2dc6b35e8aeb46eeba5f0c6b3690faa
TicoTaco <18843500008@163.com>
2020-07-22 03:01:51 +0800

    nice I finished

--- day2/am/filter.js
+++ day2/am/filter.js
@@ -5,6 +5,10 @@ const arr = [7, 8, 15, 14, 13, 70, 44, 35, 88, 77, false, "7", "hi", null]
 
 const multOf7 = el => {
   // TODO: Check if el is a multOf7
+  if (el % 7 == 0 && typeof el != "string"){
+    return el
+  }
+  console.log(el)
 }
 
 // Challenge: Implement the filter method below!
@@ -16,5 +20,4 @@ const multOf7 = el => {
 // }
 
 const newArr = arr.filter(multOf7)
-
 console.log(newArr)
